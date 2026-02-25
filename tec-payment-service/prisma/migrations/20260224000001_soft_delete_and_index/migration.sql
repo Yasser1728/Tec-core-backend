a94a7ce0eb2e3383a9297fba21fa3c1fc5f68c69
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-02-25 12:22:58 +0200

    Delete tec-payment-service/prisma/migrations/20260224000001_soft_delete_and_index directory

--- tec-payment-service/prisma/migrations/20260224000001_soft_delete_and_index/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable: add soft-delete column to payments
-ALTER TABLE "payments" ADD COLUMN "deleted_at" TIMESTAMP(3);
-
--- CreateIndex: support paginated payment history ordered by time
-CREATE INDEX "payments_created_at_idx" ON "payments"("created_at");
