d9491cb17e7df00c9088dbedd11db9848df89fb2
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-18 07:47:47 +0200

    Delete tec-auth-service/src/modules/auth/dto directory

--- tec-auth-service/src/modules/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-// src/modules/auth/dto/login.dto.ts
-
-export class LoginDto {
-  email?: string;
-  password?: string;
-  pi_uid?: string;
-  device?: string;
-  ip_address?: string;
-}
--- tec-auth-service/src/modules/auth/dto/register.dto.ts
+++
@@ -1,8 +0,0 @@
-// src/modules/auth/dto/register.dto.ts
-
-export class RegisterDto {
-  email?: string;
-  password?: string;
-  pi_uid?: string;      // هذا الحقل ضروري لحل خطأ Property 'pi_uid' does not exist
-  pi_username?: string; // وهذا أيضاً
-}
