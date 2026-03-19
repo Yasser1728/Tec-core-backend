c0edc453f7293385add565fa4fcc960793512c64
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-19 21:26:16 +0200

    Create AppModule with global config and modules

---
+++ tec-identity-service/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from './prisma/prisma.module';
+import { IdentityModule } from './modules/identity/identity.module';
+import { HealthModule } from './modules/health/health.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    PrismaModule,
+    IdentityModule,
+    HealthModule,
+  ],
+})
+export class AppModule {}
