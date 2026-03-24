1ab502514bbb69563d7bfe95664d093fd2a75564
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-24 09:39:40 +0200

    Remove JwtModule from OrderModule
    Removed JwtModule import and configuration from OrderModule.

--- tec-commerce-service/src/modules/order/order.module.ts
+++ tec-commerce-service/src/modules/order/order.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { OrdersController } from './order.controller';
 import { OrdersService }    from './order.service';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret:     process.env.JWT_SECRET ?? 'secret',
-      signOptions: { expiresIn: '24h' },
-    }),
-  ],
   controllers: [OrdersController],
   providers:   [OrdersService],
   exports:     [OrdersService],
