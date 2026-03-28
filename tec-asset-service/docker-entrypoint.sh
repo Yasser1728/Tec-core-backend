dd4b6ece40f2d4b858806ecf5aac0d47bbf87522
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-28 13:59:42 +0200

    Replace Prisma migration with db push command

--- tec-asset-service/docker-entrypoint.sh
+++ tec-asset-service/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Running Prisma migrations..."
-npx prisma migrate deploy --schema=./prisma/schema.prisma
+echo "Running Prisma db push..."
+npx prisma db push --schema=./prisma/schema.prisma --accept-data-loss
 
 echo "Starting Asset Service..."
 exec node dist/main
