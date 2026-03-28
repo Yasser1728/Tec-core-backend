37097c4b99489135b5d294898b24fa61655826ab
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-28 17:40:17 +0200

    Change entrypoint to use main.js instead of main

--- tec-asset-service/docker-entrypoint.sh
+++ tec-asset-service/docker-entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running Prisma db push..."
 npx prisma db push --schema=./prisma/schema.prisma --accept-data-loss
 
 echo "Starting Asset Service..."
-exec node dist/main
+exec node dist/main.js
