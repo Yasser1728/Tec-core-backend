3b24641c43c9aba9bd6da8e3ee53c77eb786b766
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-28 10:56:17 +0200

    Add test environment setup in setup.ts

---
+++ tec-asset-service/__tests__/setup.ts
@@ -0,0 +1,4 @@
+process.env.NODE_ENV        = 'test';
+process.env.DATABASE_URL    = 'postgresql://test:test@localhost:5432/testdb';
+process.env.INTERNAL_SECRET = 'test-internal-secret';
+process.env.PORT            = '5004';
