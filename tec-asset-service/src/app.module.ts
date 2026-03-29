2db03ec56c54f3d8097d9107fec5160bba0e15f3
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-29 17:14:15 +0200

    Add Marketplace and Health modules to AppModule

--- tec-asset-service/src/app.module.ts
+++ tec-asset-service/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AssetModule } from './modules/asset/asset.module';
+import { MarketplaceModule } from './modules/marketplace/marketplace.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AssetModule,
+    MarketplaceModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
