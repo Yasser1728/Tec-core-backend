1a2b634382d28a874c5e6e963b673aa3c2758ea7
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-01 19:38:48 +0200

    Add DeviceTokenController and FcmService to NotificationModule

--- tec-notification-service/src/modules/notification/notification.module.ts
+++ tec-notification-service/src/modules/notification/notification.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { NotificationController } from './notification.controller';
-import { NotificationService } from './notification.service';
+import { Module }                  from '@nestjs/common';
+import { NotificationController }  from './notification.controller';
+import { DeviceTokenController }   from './device-token.controller';
+import { NotificationService }     from './notification.service';
+import { FcmService }              from './fcm.service';
 
 @Module({
-  controllers: [NotificationController],
-  providers: [NotificationService],
-  exports: [NotificationService],
+  controllers: [NotificationController, DeviceTokenController],
+  providers:   [NotificationService, FcmService],
+  exports:     [NotificationService],
 })
 export class NotificationModule {}
