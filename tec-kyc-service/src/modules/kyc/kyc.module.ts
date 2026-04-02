fffff09422f74e694f8efc43dc606b43c263d90a
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-02 19:49:03 +0200

    Format imports and add JWT module to KYC module

--- tec-kyc-service/src/modules/kyc/kyc.module.ts
+++ tec-kyc-service/src/modules/kyc/kyc.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module }        from '@nestjs/common';
+import { JwtModule }     from '@nestjs/jwt';
 import { KycController } from './kyc.controller';
-import { KycService } from './kyc.service';
+import { KycService }    from './kyc.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'tec-dev-secret',
+    }),
+  ],
   controllers: [KycController],
-  providers: [KycService],
-  exports: [KycService],
+  providers:   [KycService],
+  exports:     [KycService],
 })
 export class KycModule {}
