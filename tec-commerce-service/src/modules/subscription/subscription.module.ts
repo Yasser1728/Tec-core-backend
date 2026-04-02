158b373c441a62cb40a4641c6b3fe361e1036a3b
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-02 19:57:50 +0200

    Update subscription.module.ts

--- tec-commerce-service/src/modules/subscription/subscription.module.ts
+++ tec-commerce-service/src/modules/subscription/subscription.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
-import { SubscriptionController } from './subscription.controller';
-import { SubscriptionService }    from './subscription.service';
+import { Module }                  from '@nestjs/common';
+import { JwtModule }               from '@nestjs/jwt';
+import { SubscriptionController }  from './subscription.controller';
+import { SubscriptionService }     from './subscription.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'tec-dev-secret',
+    }),
+  ],
   controllers: [SubscriptionController],
   providers:   [SubscriptionService],
   exports:     [SubscriptionService],
