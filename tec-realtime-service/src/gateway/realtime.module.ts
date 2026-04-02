d9be8a4a52b9e358671f645901b87466025491ba
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-02 19:54:13 +0200

    Add JWT module to RealtimeModule imports

--- tec-realtime-service/src/gateway/realtime.module.ts
+++ tec-realtime-service/src/gateway/realtime.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module }          from '@nestjs/common';
+import { JwtModule }       from '@nestjs/jwt';
 import { RealtimeGateway } from './realtime.gateway';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'tec-dev-secret',
+    }),
+  ],
   providers: [RealtimeGateway],
-  exports: [RealtimeGateway],
+  exports:   [RealtimeGateway],
 })
 export class RealtimeModule {}
