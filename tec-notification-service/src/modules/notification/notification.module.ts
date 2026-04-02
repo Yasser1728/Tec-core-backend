547fb528e40af36ccee1e984ce6036ede48c756e
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-02 19:47:40 +0200

    Add JWT module to NotificationModule imports

--- tec-notification-service/src/modules/notification/notification.module.ts
+++ tec-notification-service/src/modules/notification/notification.module.ts
@@ -1,10 +1,16 @@
 import { Module }                  from '@nestjs/common';
+import { JwtModule }               from '@nestjs/jwt';
 import { NotificationController }  from './notification.controller';
 import { DeviceTokenController }   from './device-token.controller';
 import { NotificationService }     from './notification.service';
 import { FcmService }              from './fcm.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'tec-dev-secret',
+    }),
+  ],
   controllers: [NotificationController, DeviceTokenController],
   providers:   [NotificationService, FcmService],
   exports:     [NotificationService],
