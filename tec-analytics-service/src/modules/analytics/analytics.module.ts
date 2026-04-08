048a5afe5816756816dc99e576678aa520b3b6b8
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-08 11:36:48 +0200

    Refactor imports and formatting in analytics.module.ts

--- tec-analytics-service/src/modules/analytics/analytics.module.ts
+++ tec-analytics-service/src/modules/analytics/analytics.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module }     from '@nestjs/common';
+import { JwtModule }  from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AnalyticsController } from './analytics.controller';
-import { AnalyticsService } from './analytics.service';
+import { AnalyticsService }    from './analytics.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports:    [ConfigModule],
+      inject:     [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret:       config.get<string>('JWT_SECRET'),
+        signOptions:  { algorithm: 'HS256' },
+      }),
+    }),
+  ],
   controllers: [AnalyticsController],
-  providers: [AnalyticsService],
-  exports: [AnalyticsService],
+  providers:   [AnalyticsService],
+  exports:     [AnalyticsService],
 })
 export class AnalyticsModule {}
