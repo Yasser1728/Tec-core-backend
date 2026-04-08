712307412a8151f4b00f72e284b72221da40fd39
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-08 14:49:03 +0200

    Fix formatting in internal-auth.ts

--- shared/src/middleware/internal-auth.ts
+++ shared/src/middleware/internal-auth.ts
@@ -1,4 +1,4 @@
-import { timingSafeEqual }               from 'crypto';
+import { timingSafeEqual }                from 'crypto';
 import { Request, Response, NextFunction } from 'express';
 
 export const validateInternalKey = (
